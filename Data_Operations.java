7f1b1cbbde513e98e97674b7f0278da67b2507d6
Nithish-2007-hub <nithish07trt@gmail.com>
2026-01-27 20:49:22 +0530

    Add files via upload

---
+++ Data_Operations.java
@@ -0,0 +1,23 @@
+class Data_Operations
+{
+public static void main(String args[])
+{
+int a=2;
+int b=4;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a/b);
+System.out.println(a%b);
+a++;
+System.out.println(a);
+b--;
+System.out.println(b);
+System.out.println(a>b);
+System.out.println(a<b);
+System.out.println(a<=b);
+System.out.println(a>=b);
+System.out.println(a!=b);
+System.out.println(a==b);
+
+}
+}
